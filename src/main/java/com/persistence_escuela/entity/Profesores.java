4b26af60daec262331ec56b9b54fb6eef3994346
kevin <kevin.cruz.garc@gmail.com>
2023-09-25 09:54:43 -0600

    porfesores

--- src/main/java/com/persistence_escuela/entity/Profesores.java
+++ src/main/java/com/persistence_escuela/entity/Profesores.java
@@ -20,7 +20,7 @@ public class Profesores implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "profAuto")
-	@SequenceGenerator(sequenceName = "profAuto", name = "SQ_PROFESOR", allocationSize = 1)
+	@SequenceGenerator(sequenceName ="profAuto", name = "SQ_PROFESORES", allocationSize = 1)
 	@Column(name = "ID_PROFESOR", columnDefinition = "NUMBER")
 	private int idProfesor;
  
