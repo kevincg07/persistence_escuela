8dbd9cd3f14316e25e98e0b105a2c9df253fc8a6
Kevin Garcia <kevin.cruz.garc@gmail.com>
2023-10-04 15:31:30 -0600

    validaciones en alumnos

--- src/main/java/com/persistence_escuela/request/EstudiantesRequest.java
+++ src/main/java/com/persistence_escuela/request/EstudiantesRequest.java
@@ -2,16 +2,24 @@ package com.persistence_escuela.request;
 
 import java.time.LocalDateTime;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+
 import com.persistence_escuela.entity.Carreras;
 
 public class EstudiantesRequest {
 	
 	private int idEstudiante;
+	@NotBlank(message = "es obligatorio escribir el nombre del estudiante")
 	private String nombre;
+	@NotBlank(message = "se necesita un numero de telefono para registrarse")
 	private String telefono;
+	@NotBlank(message = "se necesita un email para poder registrar un nuevo alumno")
 	private String email;
+	//@NotBlank(message = "no puedes regstrar al alumno si dejas vacia la fecha de ingreso")
 	private LocalDateTime fechaIngreso;
 	private Carreras carrera;
+	@Digits(integer = 4 /*precision*/, fraction = 2 /*scale*/)
 	private double colegiatura;
 	
 	public EstudiantesRequest() {}
