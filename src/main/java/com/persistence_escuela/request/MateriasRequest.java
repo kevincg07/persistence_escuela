63ff2f13b4d720b5215c5090865d117d6d9672b9
Kevin Garcia <kevin.cruz.garc@gmail.com>
2023-10-04 11:46:30 -0600

    se agregaron validaciones en el nombre y expecion para borrar id

--- src/main/java/com/persistence_escuela/request/MateriasRequest.java
+++ src/main/java/com/persistence_escuela/request/MateriasRequest.java
@@ -1,11 +1,19 @@
 package com.persistence_escuela.request;
 
+import javax.validation.constraints.NotBlank;
+
 public class MateriasRequest {
 
 	private int idMateria;
+	@NotBlank(message = "El atributo de materia debe tener un nombre y no debe estar vacio")
 	private String materia;
 	
 	public MateriasRequest() {}
+	
+	public MateriasRequest(int idMateria, String materia) {
+		this.idMateria = idMateria;
+		this.materia = materia;
+	}
 
 	public int getIdMateria() {
 		return idMateria;
